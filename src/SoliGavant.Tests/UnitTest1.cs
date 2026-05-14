93924f43b669aa5e8a85169b367885838f2c4400
artswordgames <artswordgames@gmail.com>
2026-05-14 00:36:10 -0500

    Test/unit test 1 (#4)
    * docs: add machine setup reference guide
    
    * test: add d20 range sanity check

--- src/SoliGavant.Tests/UnitTest1.cs
+++ src/SoliGavant.Tests/UnitTest1.cs
@@ -3,8 +3,12 @@ namespace SoliGavant.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void D20Roll_ShouldBeWithinRange()
     {
+        var random = new Random();
+        int result = random.Next(1, 21);
 
+        Assert.True(result >= 1);
+        Assert.True(result <= 20);
     }
 }
